c703877febc25760649e9a5a6cee8b0697a51da0
Vitaliy <vitalikshubnikov@mail.ru>
2020-09-12 06:29:02 +0300

    init first page

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <View>
+            <Text style={styles.h1}>Пожертвования</Text>
+        </View>
+        <View style={styles.hr}></View>
+        <View style={styles.wrap}>
+            <Text style={styles.span}>
+              У Вас пока нет сборов. {'\n'} Начните доброе дело.
+            </Text>
+            <TouchableOpacity
+            style={styles.btn}
+            // onPress={onPress}
+            >
+              <Text style={styles.btn_text}>
+                Создать сбор
+              </Text>
+            </TouchableOpacity>
+          
+          <StatusBar style="auto" />
+        </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +32,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    position: 'relative',
+    height: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  wrap: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: '50%',
+  },
+  h1: {
+    fontSize: 21,
+    fontWeight: '600',
+    margin: 8.5,
+  },
+  span: {
+    textAlign: 'center',
+    color: '#818C99'
+  },
+  btn: {
+    backgroundColor: '#4986CC',
+    width: 133,
+    height: 36,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 24,
+    alignSelf: 'center',
+  },
+  btn_text: {
+    color: '#FFFFFF',
+  },
+  hr: {
+    height: 0.33,
+    backgroundColor: '#D7D8D9',
+    paddingRight: 12,
+    paddingLeft: 12,
+    marginTop: 13.5,
+    width: '90%',
+  }
 });
